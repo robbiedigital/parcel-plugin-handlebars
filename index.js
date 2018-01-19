499932b126d98be08e56cfeb1f10768e35143853
theblackbolt <jayrotek@gmail.com>
2018-01-18 22:58:37 -0700

    Plugin will now process .html files as handlebars

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = bundler => {
   // process handlebars files by .hbs and .handlebars extensions
   bundler.addAssetType('hbs', require.resolve('./lib/HbsAsset'));
   bundler.addAssetType('handlebars', require.resolve('./lib/HbsAsset'));
+  bundler.addAssetType('html', require.resolve('./lib/HbsAsset'));
 };
